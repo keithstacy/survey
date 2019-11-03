30a34b02e9d8b9b5b82b7a1f069f158e0344dedd
Keith Stacy <keith.stacy@live.com>
2019-11-03 16:41:50 -0600

    New directive class

---
+++ src/app/open-print-window.directive.spec.ts
@@ -0,0 +1,8 @@
+import { OpenPrintWindowDirective } from './open-print-window.directive';
+
+describe('OpenPrintWindowDirective', () => {
+  it('should create an instance', () => {
+    const directive = new OpenPrintWindowDirective();
+    expect(directive).toBeTruthy();
+  });
+});
